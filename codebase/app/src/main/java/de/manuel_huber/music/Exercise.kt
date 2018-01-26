c736257857306dc5d62db925f7a921b1d7af2eb7
Manuel Huber <nelo112@web.de>
2018-01-26 21:17:39 +0100

    implemented main & exercise screen

--- codebase/app/src/main/java/de/manuel_huber/music/Exercise.kt
+++ codebase/app/src/main/java/de/manuel_huber/music/Exercise.kt
@@ -13,10 +13,6 @@ class Exercise : AppCompatActivity() {
         setContentView(R.layout.activity_exercise)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
 }
--- codebase/app/src/main/java/de/manuel_huber/music/MainActivity.kt
+++ codebase/app/src/main/java/de/manuel_huber/music/MainActivity.kt
@@ -1,5 +1,6 @@
 package de.manuel_huber.music
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,7 +9,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_finger_control)
+        setContentView(R.layout.activity_main)
     }
 
+    fun startExercise(view: View) {
+        val intent = Intent(this, Exercise::class.java).apply { }
+        startActivity(intent)
+    }
 }
