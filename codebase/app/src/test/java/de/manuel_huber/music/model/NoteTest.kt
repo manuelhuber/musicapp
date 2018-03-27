de5c274f9f19ccf44fee3f1d5d9302ed6d9a07a2
Manuel Huber <dev@manuel-huber.de>
2018-03-27 17:20:34 +0200

    fixed function parameter orders

--- codebase/app/src/test/java/de/manuel_huber/music/model/NoteTest.kt
+++ codebase/app/src/test/java/de/manuel_huber/music/model/NoteTest.kt
@@ -1,7 +1,7 @@
 package de.manuel_huber.music.model
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.Assert.*
 
 class NoteTest {
 
@@ -9,60 +9,60 @@ class NoteTest {
     fun stepsUpATest() {
         val a = Note(0)
         assertEquals(a.toString(), "A")
-        assertEquals(a.steps(12).toString(), "A")
-        assertEquals(a.steps(24).toString(), "A")
-        assertEquals(a.steps(27).toString(), "C")
-        assertEquals(a.steps(30).toString(), "D#")
+        assertEquals("A", a.steps(12).toString())
+        assertEquals("A", a.steps(24).toString())
+        assertEquals("C", a.steps(27).toString())
+        assertEquals("D#", a.steps(30).toString())
     }
 
     @Test
     fun stepsUpGTest() {
         val g = Note(10)
         assertEquals(g.toString(), "G")
-        assertEquals(g.steps(1).toString(), "G#")
-        assertEquals(g.steps(2).toString(), "A")
-        assertEquals(g.steps(3).toString(), "A#")
-        assertEquals(g.steps(4).toString(), "B")
-        assertEquals(g.steps(5).toString(), "C")
+        assertEquals("G#", g.steps(1).toString())
+        assertEquals("A", g.steps(2).toString())
+        assertEquals("A#", g.steps(3).toString())
+        assertEquals("B", g.steps(4).toString())
+        assertEquals("C", g.steps(5).toString())
     }
 
     @Test
     fun stepsDownATest() {
         val a = Note(0)
         assertEquals(a.toString(), "A")
-        assertEquals(a.steps(-1).toString(), "G#")
-        assertEquals(a.steps(-2).toString(), "G")
-        assertEquals(a.steps(-3).toString(), "F#")
-        assertEquals(a.steps(-4).toString(), "F")
+        assertEquals("G#", a.steps(-1).toString())
+        assertEquals("G", a.steps(-2).toString())
+        assertEquals("F#", a.steps(-3).toString())
+        assertEquals("F", a.steps(-4).toString())
     }
 
     @Test
     fun stepsDownASharpTest() {
         val aSharp = Note(1)
         assertEquals(aSharp.toString(), "A#")
-        assertEquals(aSharp.steps(-1).toString(), "A")
-        assertEquals(aSharp.steps(-2).toString(), "G#")
-        assertEquals(aSharp.steps(-3).toString(), "G")
-        assertEquals(aSharp.steps(-4).toString(), "F#")
+        assertEquals("A", aSharp.steps(-1).toString())
+        assertEquals("G#", aSharp.steps(-2).toString())
+        assertEquals("G", aSharp.steps(-3).toString())
+        assertEquals("F#", aSharp.steps(-4).toString())
     }
 
     @Test
     fun stepsDownBTest() {
         val b = Note(2)
         assertEquals(b.toString(), "B")
-        assertEquals(b.steps(-1).toString(), "A#")
-        assertEquals(b.steps(-2).toString(), "A")
-        assertEquals(b.steps(-3).toString(), "G#")
-        assertEquals(b.steps(-4).toString(), "G")
+        assertEquals("A#", b.steps(-1).toString())
+        assertEquals("A", b.steps(-2).toString())
+        assertEquals("G#", b.steps(-3).toString())
+        assertEquals("G", b.steps(-4).toString())
     }
 
     @Test
     fun stepsDownGTest() {
         val g = Note(10)
         assertEquals(g.toString(), "G")
-        assertEquals(g.steps(-10).toString(), "A")
-        assertEquals(g.steps(-12).toString(), "G")
-        assertEquals(g.steps(-17).toString(), "D")
-        assertEquals(g.steps(-24).toString(), "G")
+        assertEquals("A", g.steps(-10).toString())
+        assertEquals("G", g.steps(-12).toString())
+        assertEquals("D", g.steps(-17).toString())
+        assertEquals("G", g.steps(-24).toString())
     }
 }
