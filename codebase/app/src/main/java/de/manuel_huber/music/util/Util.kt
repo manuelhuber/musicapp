7cec6862c64cb5c3b7dc445baff26970823e2d4a
Manuel Huber <dev@manuel-huber.de>
2018-03-27 17:20:16 +0200

    implemented util function for looping

--- codebase/app/src/main/java/de/manuel_huber/music/util/Util.kt
+++ codebase/app/src/main/java/de/manuel_huber/music/util/Util.kt
@@ -8,4 +8,25 @@ fun rndNumber(min: Int, max: Int): Int {
     return random.nextInt(max - min + 1) + min
 }
 
-fun <E> List<E>.getRandomElement() = this[random.nextInt(this.size)]
+/**
+ * Maps the number to another number in the given range
+ * Values that are higher than the maximum start at the minimum again.
+ * max + 1 = min
+ * max + 2 = min + 1
+ * etc
+ * Values below the minimum start again at the maximum
+ * min - 1 = max
+ * min - 2 = max - 1
+ */
+fun loopWithin(min: Int, max: Int, value: Int): Int {
+    if (value in min..max) return value
+    val range = (max - min + 1)
+    return if (value > max) {
+        min + (value - max - 1) % range
+    } else {
+        max - (min - value - 1) % range
+    }
+}
+
+fun <E> List<E>.getRandomElement() = this[rndNumber(0, this.size)]
+fun <E> Array<E>.getRandomElement() = this[rndNumber(0, this.size)]
