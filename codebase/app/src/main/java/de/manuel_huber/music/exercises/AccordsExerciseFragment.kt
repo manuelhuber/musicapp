aeb2d8d1045892541330c7de811d8e56c8cdcac3
Manuel Huber <dev@manuel-huber.de>
2018-04-08 12:52:25 +0200

    made a generic interval series fragment

--- codebase/app/src/main/java/de/manuel_huber/music/exercises/AccordsExerciseFragment.kt
+++ codebase/app/src/main/java/de/manuel_huber/music/exercises/AccordsExerciseFragment.kt
@@ -1,62 +1,23 @@
 package de.manuel_huber.music.exercises
 
 
-import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import de.manuel_huber.music.R
 import de.manuel_huber.music.model.Intervals
 import de.manuel_huber.music.resources.getRandomAccord
-import kotlinx.android.synthetic.main.fragment_accords_exercise.*
 
 /**
  * Displays the accord exercise.
  * Shows a note and an accord name, then the intervals of the accord and then the actual notes
  * Accords are chosen completely at random
  */
-class AccordsExerciseFragment : Fragment() {
+class AccordsExerciseFragment : IntervalSeriesExerciseFragment() {
+    override val title: Int
+        get() = R.string.accords_title
+    override val description: Int
+        get() = R.string.accords_explanation
 
-    private var next = NextShow.NOTE
-    private lateinit var accord: Intervals
-
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_accords_exercise, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        layoutExerciseButton.setOnClickListener { click() }
-        showNote()
-    }
-
-    private fun click() {
-        when (next) {
-            NextShow.NOTE -> showNote()
-            NextShow.INTERVAL -> showInterval()
-            NextShow.SOLUTIONS -> showSolution()
-        }
-    }
-
-    private fun showNote() {
-        accord = getRandomAccord(resources, context!!)
-        textExercise.text = resources.getString(R.string.accord_composite, accord.startingNote, accord.nameId)
-        textSteps.text = ""
-        textSolution.text = ""
-        next = NextShow.INTERVAL
-    }
-
-    private fun showInterval() {
-        textSteps.text = accord.steps.joinToString("-")
-        next = NextShow.SOLUTIONS
-    }
-
-    private fun showSolution() {
-        textSolution.text = accord.toString()
-        next = NextShow.NOTE
+    override fun nextInterval(): Intervals {
+        return getRandomAccord(resources, context!!)
     }
 
     companion object {
@@ -65,9 +26,4 @@ class AccordsExerciseFragment : Fragment() {
         }
     }
 
-    enum class NextShow {
-        NOTE,
-        INTERVAL,
-        SOLUTIONS
-    }
 }
--- codebase/app/src/main/java/de/manuel_huber/music/exercises/ScalesFragment.kt
+++ codebase/app/src/main/java/de/manuel_huber/music/exercises/ScalesFragment.kt
@@ -1,60 +1,24 @@
 package de.manuel_huber.music.exercises
 
 
-import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import de.manuel_huber.music.R
 import de.manuel_huber.music.model.Intervals
 import de.manuel_huber.music.resources.getRandomScale
-import kotlinx.android.synthetic.main.fragment_scales.*
 
-class ScalesFragment : Fragment() {
+class ScalesFragment : IntervalSeriesExerciseFragment() {
+    override val title: Int
+        get() = R.string.scales_title
+    override val description: Int
+        get() = R.string.scales_explanation
 
-    private var state = -1
-    private lateinit var scale: Intervals
-
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_scales, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        layoutScalesCenter.setOnClickListener { click() }
-        click()
-    }
-
-    fun click() {
-        when (state) {
-            0 -> {
-                textSteps.text = scale.steps.joinToString("-")
-                state++
-            }
-            1 -> {
-                textScale.text = scale.toString()
-                state++
-            }
-            else -> {
-                textSteps.text = ""
-                textScale.text = ""
-                newScale()
-                textExercise.text = resources.getString(R.string.accord_composite, scale.startingNote, scale.nameId)
-                state = 0
-            }
-        }
-    }
-
-    fun newScale() {
-        scale = getRandomScale(resources, context!!);
+    override fun nextInterval(): Intervals {
+        return getRandomScale(resources, context!!)
     }
 
     companion object {
         fun newInstance(): ScalesFragment {
             return ScalesFragment()
         }
+
     }
 }
