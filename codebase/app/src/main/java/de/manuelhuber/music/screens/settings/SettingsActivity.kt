b00a4e502214e26db49195fe41ba034868886fdb
Manuel Huber <dev@manuel-huber.de>
2018-10-06 14:17:12 +0200

    simplified settings activity

--- codebase/app/src/main/java/de/manuelhuber/music/screens/settings/SettingsActivity.kt
+++ codebase/app/src/main/java/de/manuelhuber/music/screens/settings/SettingsActivity.kt
@@ -1,34 +1,23 @@
 package de.manuelhuber.music.screens.settings
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import dagger.android.AndroidInjection
+import de.manuelhuber.music.screens.exerciseManagement.ExerciseManagementActivity
+import kotlinx.android.synthetic.main.activity_settings.*
 import de.manuelhuber.music.R
 
-import kotlinx.android.synthetic.main.activity_settings.*
-import javax.inject.Inject
-
-class SettingsActivity : AppCompatActivity(), SettingsContract.View {
-
-    @Inject
-    lateinit var presenter: SettingsContract.Presenter
+class SettingsActivity : AppCompatActivity() {
 
     fun exerciseManagementClicked(view: View) {
-        presenter.openExerciseManagement()
+        val intent = Intent(this, ExerciseManagementActivity::class.java).apply { }
+        startActivity(intent)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         setSupportActionBar(toolbar)
-    }
-
-    override fun openActivity(activity: Class<out Activity>) {
-        val intent = Intent(this, activity).apply { }
-        startActivity(intent)
     }
 }
